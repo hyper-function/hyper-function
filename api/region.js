9756adaefce51b121ceb48d2187411643de31744
terry-fei <i@terryfei.com>
2022-06-18 14:47:09 +0800

    change api config

--- api/region.js
+++ api/region.js
@@ -1,3 +1,7 @@
 export default function handler(request, response) {
-  response.status(200).json(request);
+  response.status(200).json({
+    headers: request.headers,
+    query: request.query,
+    cookies: request.cookies,
+  });
 }
