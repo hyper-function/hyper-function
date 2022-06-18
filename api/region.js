d9407d4355a862f1d9bb2dd334acf93563278ca3
terry-fei <i@terryfei.com>
2022-06-18 14:53:46 +0800

    format region body

--- api/region.js
+++ api/region.js
@@ -1,7 +1,9 @@
 export default function handler(request, response) {
+  const { headers } = request;
   response.status(200).json({
-    headers: request.headers,
-    query: request.query,
-    cookies: request.cookies,
+    ip: headers["x-real-ip"],
+    ipCity: headers["x-vercel-ip-city"],
+    ipCountry: headers["x-vercel-ip-country"],
+    ipRegion: headers["x-vercel-ip-country-region"],
   });
 }
