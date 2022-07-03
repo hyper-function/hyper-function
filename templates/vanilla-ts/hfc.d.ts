62497f03192392a1f5492c1098bd617f930aef11
Terry Fei <i@terryfei.com>
2022-07-03 18:17:27 +0800

    update default hfc.d.ts

--- templates/vanilla-ts/hfc.d.ts
+++ templates/vanilla-ts/hfc.d.ts
@@ -1,5 +1,33 @@
+// This is example of how to write hfc.d.ts
+// Please remove unused content
+
 interface HfcPropType {
-  attrs: {};
-  events: {};
-  slots: {};
+  attrs: {
+    /**
+     * Description for this field
+     * @default "default value"
+     */
+    name: HfcString;
+    age: HfcInt;
+    weight: HfcFloat;
+    adult: HfcBoolean;
+    any: HfcAny;
+    address: Address;
+    hobbies: HfcString[];
+  };
+  events: {
+    sleep: {
+      duration: HfcInt;
+    };
+  };
+  slots: {
+    clothes: {
+      color: HfcString;
+    };
+  };
+}
+
+interface Address {
+  country: HfcString;
+  city: HfcString;
 }
