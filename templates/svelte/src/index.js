14f7aab17e8b6a9ea0129c02643da3ff0b3f8f6b
Terry Fei <i@terryfei.com>
2022-07-21 05:59:05 +0800

    update svelte template

--- templates/svelte/src/index.js
+++ templates/svelte/src/index.js
@@ -1,8 +1,10 @@
+import props from "hfc-prop-names";
 import svelteToHfc from "svelte-to-hfc";
 import Component from "./component.svelte";
 
 export default svelteToHfc(Component, {
   tag: "div",
+  props,
   connected(container, props) {},
   disconnected() {},
 });
