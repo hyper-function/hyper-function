44ab07e0579c4362b87a81c2fd33f5855b82acfb
Terry Fei <i@terryfei.com>
2022-07-20 23:43:20 +0800

    change design of hfc

--- examples/awa-btn/src/index.js
+++ examples/awa-btn/src/index.js
@@ -1,13 +1,16 @@
 import "./index.css";
 import add from "./add";
 import logo from "./jser-logo.png";
-import propNames from "hfc-prop-names";
+import props from "hfc-prop-names";
 import svg from "./heart.svg";
+
+console.log(props);
 
 export default class HFC {
   static tag = "div";
-  static propNames = propNames;
+  static props = props;
   constructor(container, props) {
+    console.log(props);
     const btn = document.createElement("button");
     btn.classList.add("btn");
     btn.innerText = "AWA BTN!!!";
