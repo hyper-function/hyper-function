5dc318448296851f94ae492ce04db6c5414f7115
Terry Fei <i@terryfei.com>
2022-08-01 21:44:44 +0800

    add shadow and icon button to hfz-preview

--- client/src/views/Preview.vue
+++ client/src/views/Preview.vue
@@ -109,35 +109,6 @@ function renderHfcPreview() {
     sandbox.src = "/render/" + id;
     container.appendChild(sandbox);
 
-    const actions = document.createElement("div");
-    actions.style.position = "relative";
-
-    actions.style.visibility = "hidden";
-    actions.classList.add("hfz-preview-actions");
-
-    const openBtn = document.createElement("button");
-    openBtn.innerText = "OPEN";
-    openBtn.addEventListener("click", () => {
-      window.open("/render/" + id, "_blank");
-    });
-
-    const refreshBtn = document.createElement("button");
-    refreshBtn.innerText = "REFRESH";
-    refreshBtn.addEventListener("click", () => {
-      (<any>sandbox).iFrameResizer.sendMessage({ action: "refresh" });
-    });
-
-    actions.appendChild(openBtn);
-    actions.appendChild(refreshBtn);
-
-    container.appendChild(actions);
-    container.addEventListener("mouseenter", () => {
-      actions.style.visibility = "visible";
-    });
-    container.addEventListener("mouseleave", () => {
-      actions.style.visibility = "hidden";
-    });
-
     iframeResize(
       {
         log: false,
@@ -152,6 +123,28 @@ function renderHfcPreview() {
       },
       sandbox
     );
+
+    const actions = document.createElement("div");
+    actions.classList.add("hfz-preview-actions");
+
+    const openBtn = document.createElement("button");
+    openBtn.title = "Open in new tab";
+    openBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22px" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h5c.55 0 1-.45 1-1s-.45-1-1-1H5c-1.11 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-.55-.45-1-1-1s-1 .45-1 1v5c0 .55-.45 1-1 1zM14 4c0 .55.45 1 1 1h2.59l-9.13 9.13c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L19 6.41V9c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1h-5c-.55 0-1 .45-1 1z"/></svg>`;
+    openBtn.addEventListener("click", () => {
+      window.open("/render/" + id, "_blank");
+    });
+
+    const refreshBtn = document.createElement("button");
+    refreshBtn.title = "Refresh";
+    refreshBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24px" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17.65 6.35c-1.63-1.63-3.94-2.57-6.48-2.31-3.67.37-6.69 3.35-7.1 7.02C3.52 15.91 7.27 20 12 20c3.19 0 5.93-1.87 7.21-4.56.32-.67-.16-1.44-.9-1.44-.37 0-.72.2-.88.53-1.13 2.43-3.84 3.97-6.8 3.31-2.22-.49-4.01-2.3-4.48-4.52C5.31 9.44 8.26 6 12 6c1.66 0 3.14.69 4.22 1.78l-1.51 1.51c-.63.63-.19 1.71.7 1.71H19c.55 0 1-.45 1-1V6.41c0-.89-1.08-1.34-1.71-.71l-.64.65z"/></svg>`;
+    refreshBtn.addEventListener("click", () => {
+      (<any>sandbox).iFrameResizer.sendMessage({ action: "refresh" });
+    });
+
+    actions.appendChild(openBtn);
+    actions.appendChild(refreshBtn);
+
+    container.appendChild(actions);
   });
 }
 </script>
